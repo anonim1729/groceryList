f45df2abbee8abea14441bcd1f448b035d03f055
Naren <coding.demon.naren@gmail.com>
2024-02-12 09:54:15 +0530

    second commit & modified app.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ["naren", "darshan", "sachin", "anirudh"];
+    const i = Math.floor(Math.random() * 4);
+    return names[i];
+  }
+  // const name = "Naren";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to alter and save to see changes.
+          Hello {handleNameChange()}!
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learning React
-        </a>
+        </a> */}
+        {/* <p>{name}</p> */}
       </header>
+
     </div>
   );
 }
