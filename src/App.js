c524073993afc450950dede0fb3b37bf29540dd4
Naren <coding.demon.naren@gmail.com>
2024-02-12 09:36:53 +0530

    first tut | modified app.js and index.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to alter and save to see changes.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
         </a>
       </header>
     </div>
