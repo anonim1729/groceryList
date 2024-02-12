a1afa298f3883de91d5f5e93f1cbcd7e9bdbf850
Naren <coding.demon.naren@gmail.com>
2024-02-12 16:12:48 +0530

    added event handling functions on content.js and created Header.css file

--- src/Content.js
+++ src/Content.js
@@ -5,11 +5,23 @@ const Content = () => {
         return names[i];
     }
 
+    const handleClick = () => {
+        console.log("You have clicked me!");
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} has clicked the button`);
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    }
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick()}>Click me</button>
+            <button onClick={() => handleClick2('Naren')}>click here</button>
+            <button onClick={(e) => { handleClick3(e) }}>Event</button>
         </main>
     )
 }
